ae52d0aef245454fe259a220bff13a2a4ee43507
Andre Zoledziowski <az@gft.eu>
2015-04-07 15:35:19 +0200

    Workarounded AccessViolationException on shutdown by catching it

--- src/Vlc.DotNet.Core.Interops/VlcManager.ReleaseMediaPlayer.cs
+++ src/Vlc.DotNet.Core.Interops/VlcManager.ReleaseMediaPlayer.cs
@@ -5,6 +5,7 @@ namespace Vlc.DotNet.Core.Interops
 {
     public sealed partial class VlcManager
     {
+        [System.Runtime.ExceptionServices.HandleProcessCorruptedStateExceptions]
         public void ReleaseMediaPlayer(VlcMediaPlayerInstance mediaPlayerInstance)
         {
             if (mediaPlayerInstance == IntPtr.Zero)
@@ -13,6 +14,11 @@ namespace Vlc.DotNet.Core.Interops
             {
                 GetInteropDelegate<ReleaseMediaPlayer>().Invoke(mediaPlayerInstance);
             }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debugger.Log(0, "Exception", "Vlc.DotNet.Core.Interops.VlcManager.ReleaseMediaPlayer threw an Exception: " + ex.Message + Environment.NewLine);
+            }
+
             finally
             {
                 mediaPlayerInstance.Pointer = IntPtr.Zero;
